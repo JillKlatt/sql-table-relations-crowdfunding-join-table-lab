ad658047e8b4faf29c8cc553a32e2b63c804ac14
JillKlatt <klatt.jill@gmail.com>
2021-03-02 14:34:28 -0500

    commit tree 5fef2a4356a65f9c5edd3179865db5362c6993d8

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date FLOAT, end_date FLOAT);
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id TEXT, project_id TEXT);
